b9f10066b4687a5bdd3e4b48e5882e8b2d83abe4
emran-b <79926012+emran-b@users.noreply.github.com>
2023-04-05 15:37:06 +0100

    Add files via upload

---
+++ similarity-sniffer.py
@@ -0,0 +1,57 @@
+import openpyxl
+import streamlit as st
+import pandas as pd
+from difflib import SequenceMatcher
+global df
+
+# Header
+st.header("Similarity Sniffer V1.0")
+
+# Markdown
+st.markdown("by Emran Bahadur")
+
+# Subheader
+st.subheader("This app allows you to upload an excel file,iterate through each row comparing the PLP Copy and calculating a % similarity score.")
+
+st.write("Upload an excel file")
+
+# get the upload file
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+
+if uploaded_file is not None:
+    # read the excel file
+    df = pd.read_excel(uploaded_file)
+    
+    st.write(df)
+
+# Create a new dataframe to store the results
+results_df = pd.DataFrame(columns=['URL', 'PLP Copy', 'Similarity'])
+
+# Iterate through each row and compare the PLP Copy
+for index, row in df.iterrows():
+    url1 = row['URL']
+    plp1 = row['PLP Copy']
+    for index2, row2 in df.iterrows():
+        url2 = row2['URL']
+        plp2 = row2['PLP Copy']
+        # Check that the URLs are not the same
+        if url1 != url2:
+            # Calculate the similarity between two PLP Copy
+            ratio = SequenceMatcher(None, plp1, plp2).ratio()
+            # Append the results to the results dataframe
+            results_df = results_df.append({'URL 1': url1, 'URL 2': url2, 'Similarity': ratio*100}, ignore_index=True)
+
+# Sort the results by Similarity in ascending order
+results_df = results_df.sort_values('Similarity', ascending=True)
+
+# Ask user to set a similarity score to filter against
+st.title("Set a similarity score to filter against")
+similarity_threshold = st.number_input("Please enter a similarity score to filter against:", min_value = 0, max_value = 100, value = 0)
+
+#Filter the results to only include matches with a similarity above the user set threshold
+filtered_results = results_df[results_df['Similarity'] >= similarity_threshold]
+
+if st.button('Download Filtered Results'):
+    filtered_results.to_excel("final-output.xlsx", index=False)
+    st.success('Downloaded Filtered Results')
+    st.balloons()
